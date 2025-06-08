b6e4cb95e8a5a8182ac9da4929d5e0d9398b6e3d
Your Name <your.email@example.com>
2025-06-08 11:44:30 +0545

    install: react query and devtools

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,26 @@ import { ThemeProvider } from "./context/theme-provider";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import WeatherDashboard from "./pages/WeatherDashboard";
 import CityPages from "./pages/CityPages";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-
+const queryClient = new QueryClient();
 function App() {
   return (
-      <BrowserRouter>
-        <ThemeProvider defaultTheme="dark">
-          <Layout>
-            <Routes>
-              <Route path="/" element={<WeatherDashboard/>}/>
-              <Route path="/city/:cityName" element={<CityPages/>}/>
-            </Routes>
-          </Layout>
-        </ThemeProvider>
-      </BrowserRouter>
+       <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ThemeProvider defaultTheme="dark">
+            <Layout>
+              <Routes>
+                <Route path="/" element={<WeatherDashboard/>}/>
+                <Route path="/city/:cityName" element={<CityPages/>}/>
+              </Routes>
+            </Layout>
+          </ThemeProvider>
+        </BrowserRouter>
+         <ReactQueryDevtools initialIsOpen={false} />
+       </QueryClientProvider>
+
   );
 }
 
